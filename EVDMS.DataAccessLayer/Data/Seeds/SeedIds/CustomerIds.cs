453d83c1060a79613d10d1427fd85d25fe055940
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-09-22 19:05:45 +0700

    ci(workflow): use dotnet restore instead of installing tools globally

--- EVDMS.DataAccessLayer/Data/Seeds/SeedIds/CustomerIds.cs
+++ EVDMS.DataAccessLayer/Data/Seeds/SeedIds/CustomerIds.cs
@@ -3,7 +3,8 @@ namespace EVDMS.DataAccessLayer.Data.Seeds.SeedIds
     public static class CustomerIds
     {
         public static readonly Guid NguyenVanAn = Guid.Parse(
-            "10000000-0000-0000-0000-000000000001");
+            "10000000-0000-0000-0000-000000000001"
+        );
         public static readonly Guid TranThiBichNgoc = Guid.Parse(
             "10000000-0000-0000-0000-000000000002"
         );
