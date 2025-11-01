e56651eac867be1a849669930889b53b2562e72b
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-11-01 11:34:05 +0700

    feat(user): add suspend user option

--- src/EVDMS.Common/Dtos/UserDtos.cs
+++ src/EVDMS.Common/Dtos/UserDtos.cs
@@ -55,5 +55,6 @@ namespace EVDMS.Common.Dtos
         [EmailAddress]
         public string? Email { get; set; }
         public UserRole? Role { get; set; }
+        public bool? IsActive { get; set; }
     }
 }
